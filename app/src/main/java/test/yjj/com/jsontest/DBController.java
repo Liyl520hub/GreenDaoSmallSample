9d4bbd7259bfe9408df4c7c54b48f371784ab7ab
liyalei <lyl@jqrjl.com>
2018-11-09 10:38:25 +0800

    增加数据库及操作的工具类

---
+++ app/src/main/java/test/yjj/com/jsontest/DBController.java
@@ -0,0 +1,157 @@
+package test.yjj.com.jsontest;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.List;
+
+/**
+ * @author joy
+ * <p>
+ * created 2018/11/9
+ * <p>
+ * class use: 数据操作工具类
+ */
+public class DBController {
+    /**
+     * Helper 获取Helper对象
+     */
+    private DaoMaster.DevOpenHelper mHelper;
+    /**
+     * 数据库
+     */
+    private SQLiteDatabase db;
+    /**
+     * 数据库名
+     */
+    private String dbName = "dataCache.db";
+    /**
+     * DaoMaster
+     */
+    private DaoMaster mDaoMaster;
+    /**
+     * DaoSession
+     */
+    private DaoSession mDaoSession;
+    /**
+     * 上下文
+     */
+    private Context context;
+    /**
+     * dao
+     */
+    private final DataCacheDao dataCacheDao;
+
+    private static DBController mDbController;
+
+    /**
+     * 获取单例
+     */
+    public static DBController getInstance(Context context) {
+        if (mDbController == null) {
+            synchronized (DBController.class) {
+                if (mDbController == null) {
+                    mDbController = new DBController(context);
+                }
+            }
+        }
+        return mDbController;
+    }
+
+    /**
+     * 初始化
+     *
+     * @param context
+     */
+    public DBController(Context context) {
+        this.context = context;
+        mHelper = new DaoMaster.DevOpenHelper(context, dbName, null);
+        mDaoMaster = new DaoMaster(getWritableDatabase());
+        mDaoSession = mDaoMaster.newSession();
+        dataCacheDao = mDaoSession.getDataCacheDao();
+    }
+
+    /**
+     * 获取可读数据库
+     */
+    private SQLiteDatabase getReadableDatabase() {
+        if (mHelper == null) {
+            mHelper = new DaoMaster.DevOpenHelper(context, dbName, null);
+        }
+        SQLiteDatabase db = mHelper.getReadableDatabase();
+        return db;
+    }
+
+    /**
+     * 获取可写数据库
+     *
+     * @return
+     */
+    private SQLiteDatabase getWritableDatabase() {
+        if (mHelper == null) {
+            mHelper = new DaoMaster.DevOpenHelper(context, dbName, null);
+        }
+        SQLiteDatabase db = mHelper.getWritableDatabase();
+        return db;
+    }
+
+    /**
+     * 会自动判定是插入还是替换
+     *
+     * @param dataCache
+     */
+    public void insertOrReplace(DataCache dataCache) {
+        dataCacheDao.insertOrReplace(dataCache);
+    }
+
+    /**
+     * 插入一条记录，表里面要没有与之相同的记录
+     *
+     * @param dataCache
+     */
+    public long insert(DataCache dataCache) {
+        return dataCacheDao.insert(dataCache);
+    }
+
+    /**
+     * 更新数据
+     *
+     * @param urlName  要更新的url地址
+     * @param jsonData 更新的内容
+     */
+    public void update(String urlName, String jsonData) {
+        //拿到之前的记录
+        DataCache mOldPersonInfor = dataCacheDao.queryBuilder().where(DataCacheDao.Properties.CacheUrl.eq(urlName)).build().unique();
+        if (mOldPersonInfor != null) {
+            mOldPersonInfor.setCacheConent(jsonData);
+            dataCacheDao.update(mOldPersonInfor);
+        }
+    }
+
+    /**
+     * 按条件查询数据
+     */
+    public DataCache searchByWhere(String urlName) {
+        List<DataCache> personInfors = (List<DataCache>) dataCacheDao.queryBuilder().where(DataCacheDao.Properties.CacheUrl.eq(urlName)).build().unique();
+        return personInfors.get(0);
+    }
+
+    /**
+     * 查询所有数据
+     */
+    public List<DataCache> searchAll() {
+        List<DataCache> personInfors = dataCacheDao.queryBuilder().list();
+        return personInfors;
+    }
+
+    /**
+     * 删除数据
+     *
+     * @param urlName 要删除的数据的url地址
+     */
+    public void delete(String urlName) {
+        dataCacheDao.queryBuilder().where(DataCacheDao.Properties.CacheUrl.eq(urlName)).buildDelete().executeDeleteWithoutDetachingEntities();
+    }
+}
+
+
---
+++ app/src/main/java/test/yjj/com/jsontest/DataCache.java
@@ -0,0 +1,83 @@
+package test.yjj.com.jsontest;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Index;
+import org.greenrobot.greendao.annotation.Generated;
+
+/**
+ * @author joy
+ * <p>
+ * created 2018/11/9
+ * <p>
+ * class use:
+ */
+@Entity
+public class DataCache {
+    /**
+     * 设置自增长
+     */
+    @Id(autoincrement = true)
+    private Long id;
+    /**
+     * //设置唯一性
+     */
+    @Index(unique = true)
+    /**
+     * 缓存的url
+     */
+    private String cacheUrl;
+    /**
+     * 缓存的内容  json串
+     */
+    private String cacheConent;
+    /**
+     * 缓存的时间
+     */
+    private String cacheTime;
+
+    @Generated(hash = 488346778)
+    public DataCache(Long id, String cacheUrl, String cacheConent,
+                     String cacheTime) {
+        this.id = id;
+        this.cacheUrl = cacheUrl;
+        this.cacheConent = cacheConent;
+        this.cacheTime = cacheTime;
+    }
+
+    @Generated(hash = 24611778)
+    public DataCache() {
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCacheUrl() {
+        return this.cacheUrl;
+    }
+
+    public void setCacheUrl(String cacheUrl) {
+        this.cacheUrl = cacheUrl;
+    }
+
+    public String getCacheConent() {
+        return this.cacheConent;
+    }
+
+    public void setCacheConent(String cacheConent) {
+        this.cacheConent = cacheConent;
+    }
+
+    public String getCacheTime() {
+        return this.cacheTime;
+    }
+
+    public void setCacheTime(String cacheTime) {
+        this.cacheTime = cacheTime;
+    }
+}
