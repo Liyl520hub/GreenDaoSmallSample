92ffa94bbe75eb530a072bed6bed05d59bc9e1f8
liyalei <lyl@jqrjl.com>
2018-11-09 11:10:29 +0800

    解决报错

--- app/src/main/java/test/yjj/com/jsontest/DataCache.java
+++ app/src/main/java/test/yjj/com/jsontest/DataCache.java
@@ -35,49 +35,39 @@ public class DataCache {
      * 缓存的时间
      */
     private long cacheTime;
-
-    @Generated(hash = 488346778)
-    public DataCache(Long id, String cacheUrl, String cacheConent,
-                     long cacheTime) {
+    @Generated(hash = 1570312683)
+    public DataCache(Long id, String cacheUrl, String cacheConent, long cacheTime) {
         this.id = id;
         this.cacheUrl = cacheUrl;
         this.cacheConent = cacheConent;
         this.cacheTime = cacheTime;
     }
-
     @Generated(hash = 24611778)
     public DataCache() {
     }
-
     public Long getId() {
         return this.id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getCacheUrl() {
         return this.cacheUrl;
     }
-
     public void setCacheUrl(String cacheUrl) {
         this.cacheUrl = cacheUrl;
     }
-
     public String getCacheConent() {
         return this.cacheConent;
     }
-
     public void setCacheConent(String cacheConent) {
         this.cacheConent = cacheConent;
     }
-
     public long getCacheTime() {
         return this.cacheTime;
     }
-
     public void setCacheTime(long cacheTime) {
         this.cacheTime = cacheTime;
     }
+    
 }
